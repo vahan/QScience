69edbacb91111c44d8c199f607058bc653ab30bc
unknown <Vahan@.(none)>
2011-10-05 13:13:59 +0200

    changed, but still doesn't work. Deactivate this module temporarily

--- qscience.js
+++ qscience.js
@@ -1,44 +1,38 @@
-(function ($) {
-	var livingscience;
+function onsearch() {
+	alert("onsubmit");
+	if(!livingscience) 
+	{
+		livingscience = new ch.ethz.livingscience.gwtclient.api.LivingScienceSearch();
+		livingscience.setMap("mapcontainer");
+	}
 	
-	$(document).ready(function() {
-    	alert("lavash");
-    	livingscience = new ch.ethz.livingscience.gwtclient.api.LivingScienceSearch();
-    	livingscience.setMap("mapcontainer");
-    });
-    
-    $("#visualscience-search-form").submit(function() {
-    	alert("onsubmit");
-    	if (!livingscience) return;
-    	$("#watchProgress").innerHTML = "Search started...";
-    	var query = $("#edit-search-button").value;
-    	console.log("query: " + query);
-    	livingscience.search(query, function(publications) {
-    		$("#watchProgress").innerHTML = "Search finished.";
+	if (!livingscience) return;
+	document.getElementById("#watchProgress").innerHTML = "Search started...";
+	var query = document.getElementById("#edit-search-button").value;
+	console.log("query: " + query);
+	livingscience.search(query, function(publications) {
+		document.getElementById("#watchProgress").innerHTML = "Search finished.";
 
-    		var html = "";
-    		if (publications)
-    		{
-    		    for (var i=0; i<publications.length; i++)
-    			{
-    				var pub = publications[i];
-    				html += "<p>";
-    				if (pub.authors)
-    				{
-    					for (var j=0; j<pub.authors.length; j++)
-    					{
-    						var author = pub.authors[j];
-    						html += author.name;
-    						if (j < pub.authors.length - 1) html += ", ";
-    					}
-    				}
-    				html += " (" + pub.year + "): <b>" + pub.title + "</b>";
-    				html += "</p>";
-    			}
-    		}
-    		$("#searchResults").innerHTML = html;
-    	});
-    });
-})(jQuery)
-
-
+		var html = "";
+		if (publications)
+		{
+		    for (var i=0; i<publications.length; i++)
+			{
+				var pub = publications[i];
+				html += "<p>";
+				if (pub.authors)
+				{
+					for (var j=0; j<pub.authors.length; j++)
+					{
+						var author = pub.authors[j];
+						html += author.name;
+						if (j < pub.authors.length - 1) html += ", ";
+					}
+				}
+				html += " (" + pub.year + "): <b>" + pub.title + "</b>";
+				html += "</p>";
+			}
+		}
+		document.getElementById("#searchResults").innerHTML = html;
+	});
+}
